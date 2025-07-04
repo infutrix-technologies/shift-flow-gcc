aae7ce2492b664d833c5355986266dce9fc2dae3
CodeVerseByAnuj <anuj.mmmut@gmail.com>
2025-07-04 18:39:44 +0530

    added Banner

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import About from "./pages/About";
 import Contact from "./pages/Contact";
+import Terms from "./pages/Terms";
 
 // Service Pages
 import LandFreight from "./pages/services/LandFreight";
@@ -27,6 +28,10 @@ const App = () => (
           <Route path="/" element={<Index />} />
           <Route path="/about" element={<About />} />
           <Route path="/contact" element={<Contact />} />
+
+          <Route path="/terms" element={<Terms />} />
+
+          {/* Catch-all for 404 Not Found */}
 
           {/* Service Routes */}
           <Route path="/services/land-freight" element={<LandFreight />} />
