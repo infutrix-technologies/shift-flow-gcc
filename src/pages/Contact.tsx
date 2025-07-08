084d77b8d317a4485f2abdaed3cbfd0fc8c65a7a
CodeVerseByAnuj <anuj.mmmut@gmail.com>
2025-07-08 17:55:31 +0530

    update email and phone

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -28,8 +28,8 @@ const Contact = () => {
       city: "Dubai",
       country: "UAE",
       address: "Dubai Investments Park Office",
-      phone: "+971-4-123-4567",
-      email: "dubai@example.com",
+      phone: "+971 52 934 9419",
+      email: "info@gravityshift.ae",
       hours: "Mon–Fri: 9am–6pm",
       mapEmbedUrl:
         "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d28931.14292262838!2d55.14478759561374!3d24.9867631814215!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3e5f728492c989b3%3A0xacfcb5c405ca278!2sDubai%20Investments%20Park%20Office!5e0!3m2!1sen!2sin!4v1746097566542!5m2!1sen!2sin",
